9ed103bcf72f8dbbbadabd466df0c224c92efacd
satoru814 <satoru-sakamoto@c-fo.com>
2023-03-10 17:18:17 +0900

    fix-GoogleApiClient

--- langchain/document_loaders/youtube.py
+++ langchain/document_loaders/youtube.py
@@ -79,7 +79,8 @@ class GoogleApiClient:
                 "to use the Google Drive loader"
             )
 
-        # creds = None
+        creds = None
+        print(self.service_account_path)
         if self.service_account_path.exists():
             return service_account.Credentials.from_service_account_file(
                 str(self.service_account_path)
